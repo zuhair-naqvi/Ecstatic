9d554062270547f8a84e0e7afb1e0c9a0fea9ea9
Freelancer <freelance@performance.local>
2014-04-24 13:00:35 +1000

    indentation

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -6,39 +6,39 @@ require_once APP.'/Wishes/View/stencil.xhp';
 
 class BaseController 
 {
-	public function __construct()
-	{
-		$this->publishRoutes();
-	}
+  public function __construct()
+  {
+    $this->publishRoutes();
+  }
 
-	public function publishRoutes(): void
-	{
-		global $container;
-		
-		$childClass = get_called_class();
-		$reflectionObject = new \ReflectionClass($childClass);
+  public function publishRoutes(): void
+  {
+    global $container;
+    
+    $childClass = get_called_class();
+    $reflectionObject = new \ReflectionClass($childClass);
 
-		foreach($reflectionObject->getMethods() as $i => $method)
-		{
-			$className  = $childClass;
-			$methodName = $method->name;
-			 foreach ($method->info['attributes'] as $key => $value) {
-			 	if($key == 'GET' || $key == 'POST') {
-			 		// echo $value;
-			 		$container->get('Routes')->add(Map {
-			 			'name' => str_replace('\\', '_', $className . '_' . $methodName),
-			 			'method' => $key,
-			 			'pattern' => $value[0],
-			 			'controller' => $className . '::' .$methodName
-			 		});
-			 	}
-			 }
-		}
-	}
+    foreach($reflectionObject->getMethods() as $i => $method)
+    {
+      $className  = $childClass;
+      $methodName = $method->name;
+       foreach ($method->info['attributes'] as $key => $value) {
+        if($key == 'GET' || $key == 'POST') {
+          // echo $value;
+          $container->get('Routes')->add(Map {
+            'name' => str_replace('\\', '_', $className . '_' . $methodName),
+            'method' => $key,
+            'pattern' => $value[0],
+            'controller' => $className . '::' .$methodName
+          });
+        }
+       }
+    }
+  }
 
-	public function render($template, $params): void
-	{
-		include(APP.'/Wishes/View/'.$template);
-		echo $viewContent;
-	}
+  public function render($template, $params): void
+  {
+    include(APP.'/Wishes/View/'.$template);
+    echo $viewContent;
+  }
 }
--- Routing/Router.php
+++ Routing/Router.php
@@ -10,96 +10,96 @@ use Ecstatic\Routing\Route as Route;
 
 class Router {
 
-	// Todo: Match trailing slash as well
-	private ImmMap $lexerRules = ImmMap {
-									'/(?=\/).([A-Za-z0-9_]{1,16})/'  => 'T_LITERAL',
-									'/(?=\:).([A-Za-z0-9_]{1,16})/' => 'T_VARIABLE'
-								};								
-	private Vector <Route> $routes = Vector {};
-	
-	public function lex(string $pattern): array 
-	{
-		$tokens = array();
+  // Todo: Match trailing slash as well
+  private ImmMap $lexerRules = ImmMap {
+                  '/(?=\/).([A-Za-z0-9_]{1,16})/'  => 'T_LITERAL',
+                  '/(?=\:).([A-Za-z0-9_]{1,16})/' => 'T_VARIABLE'
+                };                
+  private Vector <Route> $routes = Vector {};
+  
+  public function lex(string $pattern): array 
+  {
+    $tokens = array();
 
-		foreach($this->lexerRules as $rule => $type) {	    	
-	        if(preg_match_all($rule, $pattern, $matches, PREG_OFFSET_CAPTURE)) {
-	        	$tokens = array_merge($tokens, array_map(function($match) use($type) {
-	        		return Map {
-	        			'type'   => $type,
-	        			'match'  => $match[0],
-	        			'offset' => $match[1]
-	        		};
-	        	}, $matches[1]));
-	        }
-	    }
+    foreach($this->lexerRules as $rule => $type) {        
+          if(preg_match_all($rule, $pattern, $matches, PREG_OFFSET_CAPTURE)) {
+            $tokens = array_merge($tokens, array_map(function($match) use($type) {
+              return Map {
+                'type'   => $type,
+                'match'  => $match[0],
+                'offset' => $match[1]
+              };
+            }, $matches[1]));
+          }
+      }
 
-	    usort($tokens, function($a, $b){
-	    	return ($a['offset'] < $b['offset']) ? -1 : 1;
-	    });
-	    return $tokens;
-	}
+      usort($tokens, function($a, $b){
+        return ($a['offset'] < $b['offset']) ? -1 : 1;
+      });
+      return $tokens;
+  }
 
-	public function parse(string $uri, string $method): Vector 
-	{
-		$uriTokens = explode('/', $uri);
-		array_shift($uriTokens);
-		$params = array();
-		return $this->routes->filter($route ==> { if($route->getMethod() == $method) return $route; })
-							->filter($route ==> {								
-								if(count($route->getTokens()) != count($uriTokens)) return false;
-								foreach ($route->getTokens() as $index => $token) {
-									if($route->getTokens()->get($index)->get('type') == 'T_LITERAL' && $route->getTokens()->get($index)->get('match') != $uriTokens[$index]) {
-										return false;
-									}
-									else if ($route->getTokens()->get($index)->get('type') == 'T_VARIABLE')
-									{
-										$params = array_merge($params, array($route->getTokens()->get($index)->get('match') => $uriTokens[$index]));
-									}
-								}
-								$route->setParams(Map::fromArray($params));
-								return $route;
-							});
-	}	
+  public function parse(string $uri, string $method): Vector 
+  {
+    $uriTokens = explode('/', $uri);
+    array_shift($uriTokens);
+    $params = array();
+    return $this->routes->filter($route ==> { if($route->getMethod() == $method) return $route; })
+              ->filter($route ==> {               
+                if(count($route->getTokens()) != count($uriTokens)) return false;
+                foreach ($route->getTokens() as $index => $token) {
+                  if($route->getTokens()->get($index)->get('type') == 'T_LITERAL' && $route->getTokens()->get($index)->get('match') != $uriTokens[$index]) {
+                    return false;
+                  }
+                  else if ($route->getTokens()->get($index)->get('type') == 'T_VARIABLE')
+                  {
+                    $params = array_merge($params, array($route->getTokens()->get($index)->get('match') => $uriTokens[$index]));
+                  }
+                }
+                $route->setParams(Map::fromArray($params));
+                return $route;
+              });
+  } 
 
-	public function resolve(string $uri): void 
-	{
-		global $container;
+  public function resolve(string $uri): void 
+  {
+    global $container;
 
-		$matches = $this->parse($uri, $_SERVER['REQUEST_METHOD']);
+    $matches = $this->parse($uri, $_SERVER['REQUEST_METHOD']);
 
-		if($matches->count() > 0) {
-			$firstMatch = $matches->get(0);
-			$controllerString = explode('::', $firstMatch->getController());
-			$controllerName = $controllerString[0];
-			$actionName = $controllerString[1];
-			$controllerService = $container->get('Controllers')->filter($controller ==> { if(get_class($controller) == $controllerName) return $controller; });
-			if($controllerService->count() > 0)
-			{
-				$controllerService->get(0)->$actionName($firstMatch->getParams());
-			}
-		}
-		else {
-			throw new \Exception("No matching routes", 1);
-		}
-	}
+    if($matches->count() > 0) {
+      $firstMatch = $matches->get(0);
+      $controllerString = explode('::', $firstMatch->getController());
+      $controllerName = $controllerString[0];
+      $actionName = $controllerString[1];
+      $controllerService = $container->get('Controllers')->filter($controller ==> { if(get_class($controller) == $controllerName) return $controller; });
+      if($controllerService->count() > 0)
+      {
+        $controllerService->get(0)->$actionName($firstMatch->getParams());
+      }
+    }
+    else {
+      throw new \Exception("No matching routes", 1);
+    }
+  }
 
-	public function index(): Router
-	{
-		global $container;
+  public function index(): Router
+  {
+    global $container;
 
-		foreach ($container->get('Routes') as $routeDefinition) {
-			if(!apc_exists('Route_' . $routeDefinition['name'])) {
+    foreach ($container->get('Routes') as $routeDefinition) {
+      if(!apc_exists('Route_' . $routeDefinition['name'])) {
 
-				$route = new Route($routeDefinition['name'], $routeDefinition['method'], $routeDefinition['pattern'], $routeDefinition['controller']);
-				$route->setTokens(Vector::fromArray($this->lex($route->getPattern())));				
+        $route = new Route($routeDefinition['name'], $routeDefinition['method'], $routeDefinition['pattern'], $routeDefinition['controller']);
+        $route->setTokens(Vector::fromArray($this->lex($route->getPattern())));       
 
-				apc_store('Route_' . $routeDefinition['name'], $route);
-			}
-			
-			$this->routes->add(apc_fetch('Route_' . $routeDefinition['name']));
-		}
+        apc_store('Route_' . $routeDefinition['name'], $route);
+      }
+      
+      $this->routes->add(apc_fetch('Route_' . $routeDefinition['name']));
+    }
 
-		return $this;
-	}
+    return $this;
+  }
 
 }
